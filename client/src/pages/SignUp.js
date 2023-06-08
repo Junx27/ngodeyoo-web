c16390c3f40b9e508e17fd0a1298184ec63c7956
Junx27 <103058236+Junx27@users.noreply.github.com>
2023-06-08 15:42:04 +0000

    yyy

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -37,6 +37,7 @@ function SignUp() {
       <div className="container w-50">
         Upload and Deploy in Vercel test remote from tablet
         <div>test 1 from tablet again</div>
+        <div>test kedua from tablet</div>
         <h2>Create Data</h2>
         <input
           type="text"
